7354eeff22a30349bf44d62a50d4793cf8d319f9
Jeanette Samuelsen <jeanette.samuelsen@gmail.com>
2020-10-20 14:13:31 +0200

    Added some comments for Natas - Level10->Level11

--- natas.py
+++ natas.py
@@ -4,3 +4,12 @@
 import base64
 base64.b64decode(bytearray.fromhex("3d3d516343746d4d6d6c315669563362").decode()[::-1]).decode()
 
+# Level 10-> Level 11
+# http://natas10.natas.labs.overthewire.org/
+# Relevant server-side code: 
+# if(preg_match('/[;|&]/',$key)) { print "Input contains an illegal character!"; } else { passthru("grep -i $key dictionary.txt"); }
+
+# Since the preg_match method can take an offset as an optional argument,
+# we can give the following input string ($key) to output the next level password:
+# ".* /etc/natas_webpass/* , $matches, PREG_OFFSET_CAPTURE, -1"
+# Due to setting the offset to -1, the server-side regex is only checked against the final character of the second argument.
