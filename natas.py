acb41132da5e93229a8bb5e31abdcf9bcd733055
Jeanette Samuelsen <jeanette.samuelsen@gmail.com>
2020-10-21 19:05:00 +0200

    Added some comments for Natas level 13

--- natas.py
+++ natas.py
@@ -8,8 +8,16 @@ base64.b64decode(bytearray.fromhex("3d3d516343746d4d6d6c315669563362").decode()[
 # http://natas10.natas.labs.overthewire.org/
 # Relevant server-side code: 
 # if(preg_match('/[;|&]/',$key)) { print "Input contains an illegal character!"; } else { passthru("grep -i $key dictionary.txt"); }
-
+# 
 # Since the preg_match method can take an offset as an optional argument,
 # we can give the following input string ($key) to output the next level password:
 # ".* /etc/natas_webpass/* , $matches, PREG_OFFSET_CAPTURE, -1"
 # Due to setting the offset to -1, the server-side regex is only checked against the final character of the second argument.
+
+# Level 13 -> Level 14
+# Relevant server-side code:
+# else if (! exif_imagetype($_FILES['uploadedfile']['tmp_name'])) { echo "File is not an image"; } 
+# 
+# Need to make a php file with command passthru("cat /etc/natas_webpass/*").
+# To make exif_imagetype function recognize php file as an image file, change first bytes (i.e., the file signature) to "FF D8 FF DB".
+# For uploading, change POST request parameter 'filname' from filename.jpg to filename.php.
